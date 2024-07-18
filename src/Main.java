a451caf6edf634cc8fa14749fd1102467ca167b4
LucasMorenoArg <lucas.moreno.ragni@mi.unc.edu.ar>
2024-07-18 13:21:21 -0300

    Excepciones.

--- src/Main.java
+++ src/Main.java
@@ -21,20 +21,18 @@ public class Main {
 
             //Creacion de Autores y libros
             createAuthor(authorDaoMemory);
-            getAllAuthors(authorDaoMemory);
             createBook(bookDaoMemory, authorDaoMemory);
 
-            //Actualización  y eliminacios de Autores y libros
-            Book bookActualizado = new Book("CCC", 45.00, authorDaoMemory.byId(3));
-            Book viejo = bookDaoMemory.byId(2);
-            updateAuthor(3,"Jorge Lima","JLima@gmail.com",authorDaoMemory);
-            getAllAuthors(authorDaoMemory);
-            updateBook(viejo,bookActualizado,bookDaoMemory);
+            //Actualización  y eliminacion de Autores y libros
+            updateAuthor(authorDaoMemory);
+            updateBook(bookDaoMemory, authorDaoMemory);
             getAllBooks(bookDaoMemory);
             authorOrder(authorDaoMemory);
             deleteAuthor(authorDaoMemory);
             deleteBook(bookDaoMemory);
-
+            booksSortedByPrice(bookDaoMemory);
+            booksSortedByTitle(bookDaoMemory);
+            getAllAuthors(authorDaoMemory);
 
 
         } catch (DAOException daoException){
@@ -49,40 +47,32 @@ public class Main {
 
     public static void createAuthor(AuthorDao authorDaoMemory) throws DAOException{
 
-            //Creación de Autores
-
             assert authorDaoMemory!=null;
             authorDaoMemory.create(new Author("Isabel Allende", "jlBorges@gmail.com"));
             authorDaoMemory.create(new Author("Jorge Luis Borges", "IAllende@gmail.com"));
             authorDaoMemory.create(new Author("Gabriel Garcia Marquez", "GMarquez@gmail.com"));
-
-        }
-
-
+    }
 
     public static void createBook(BookDao bookDaoMemory, AuthorDao authorDaoMemory) throws DAOException{
-
-            //Creación de Libros
 
             bookDaoMemory.create(new Book("AAA", 35.00, authorDaoMemory.byId(1)));
             bookDaoMemory.create(new Book("BBB", 30.00, authorDaoMemory.byId(2)));
             bookDaoMemory.create(new Book("CCC", 34.00, authorDaoMemory.byId(3)));
-
     }
 
-    public static void updateAuthor(Integer id,String nombre, String email, AuthorDao authorDaoMemory) throws DAOException{
+    public static void updateAuthor(AuthorDao authorDaoMemory) throws DAOException{
 
-           //Actualización de Autores
-            authorDaoMemory.update(authorDaoMemory.byId(id), nombre, email);
-
+        String nombre = "Jorge Lima";
+        String email = "JLima@gmail.com";
+        authorDaoMemory.update(authorDaoMemory.byId(3), nombre, email);
     }
 
-    public static void updateBook(Book viejo, Book actualizado, BookDao bookDaoMemory) throws DAOException{
+    public static void updateBook(BookDao bookDaoMemory, AuthorDao authorDaoMemory) throws DAOException{
 
-            bookDaoMemory.update(viejo,actualizado);
-
+        Book bookActualizado = new Book("CCC", 45.00, authorDaoMemory.byId(3));
+        Book viejo = bookDaoMemory.byId(2);
+        bookDaoMemory.update(viejo,bookActualizado);
       }
-
 
     public static void getAllAuthors(AuthorDao authorDaoMemory) throws DAOException {
 
@@ -99,13 +89,23 @@ public class Main {
 
     public static void getAllBooks(BookDao bookDaoMemory) throws DAOException{
 
-        System.out.println(bookDaoMemory.getAll());
-
-        }
+        for (Book book:bookDaoMemory.getAll())
+            System.out.println(book);
+    }
 
     public static void authorOrder(AuthorDao authorDaoMemory) throws DAOException{
 
         authorDaoMemory.getAuthorsSortedByName(Order.Desc);
+    }
+
+    public static void booksSortedByTitle(BookDao bookDaoMemory) throws DAOException{
+
+        bookDaoMemory.getBooksSortedByTitle(Order.Asc);
+    }
+
+    public static void booksSortedByPrice(BookDao bookDaoMempory) throws DAOException{
+
+        bookDaoMempory.getBooksSortedByPrice(Order.Desc);
     }
 
     public static void deleteBook(BookDao bookDaoMemory) throws DAOException{
@@ -113,5 +113,5 @@ public class Main {
         bookDaoMemory.delete(0);
     }
 
-    }
+}
 
